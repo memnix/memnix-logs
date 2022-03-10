2b161e42c3e72ae4f3a143a658ef18031fcaa31a
Yume <yume.network@protonmail.com>
2022-03-10 19:21:59 +0100

    Update
    Signed-off-by: Yume <yume.network@protonmail.com>

--- models.go
+++ models.go
@@ -5,7 +5,6 @@ import "time"
 type Log struct {
 	Type      string    `json:"type"`
 	Message   string    `json:"message"`
-	Level     string    `json:"level"`
 	Event     string    `json:"event"`
 	CreatedAt time.Time `json:"createdat"`
 	UserID    uint      `json:"userid"`
